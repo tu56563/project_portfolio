7f418d8161a123c545c2c5e0bc0a6384e59390e8
jslee <165612351+tu56563@users.noreply.github.com>
2024-08-08 16:27:43 +0900

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -3,21 +3,13 @@ $("#fullpage").fullpage({
     scrollBar : true, 
     scrollingSpeed : 100,
     // nav
-    navigation : true,
+    /* navigation : true, */
     // navigationTooltips : ["소개", "특징", "프로젝트", "연락처"],
     showActiveTooltip : true,
     menu : "nav",
     // anchors : ["intro","special","project","contact"],
     //fullpage는 scrollTrigger와 함께 이용 불가
     //fullpage(구) 제이쿼리 기능으로 애니메이션도 제이쿼리로 연결해야한다.
-    onLeave : function(index, nextIndex, direction) {
-        console.log(index, nextIndex, direction)
-        if(index == 2) {
-            $(".s3 h1").css("color", "blue")
-            //$(".s3 h1").addClass("animation class 입력")
-            $(".s3 h1").addClass("ani")
-        }
-    },
 })
 
 // sns swiper
